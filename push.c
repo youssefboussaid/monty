cbfaecd72fb835d0612b31be739ebfc0753fae65
youssefboussaid <youssbsd@gmail.com>
2021-09-22 06:34:46 -0700

    push fucntion

--- push.c
+++ push.c
@@ -10,7 +10,7 @@ stack_t *push(stack_t **stack,
 	      unsigned int line_number __attribute__((unused)))
 {
 	stack_t *new_node;
-	/* Create new node */
+
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
@@ -18,16 +18,12 @@ stack_t *push(stack_t **stack,
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	/* Initialize the new node with the value of token */
 	new_node->n = token;
-	/* New node is placed at the beginning of the list */
 	new_node->prev = NULL;
 	new_node->next = *stack;
 
-	/* If new node is not alone, update the following one */
 	if (*stack != NULL)
 		(*stack)->prev = new_node;
-	/* head points to the new node */
 	*stack = new_node;
 
 	return (new_node);
