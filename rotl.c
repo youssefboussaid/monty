b8f5fd8cca8873f894a030e15a9dc8c94f08c21b
youssefboussaid <youssbsd@gmail.com>
2021-09-22 05:45:18 -0700

    rotl function

---
+++ rotl.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * rotl - The purpose of this function is to rotate the stack to the top
+ * @stack: The pointer to the top of the stack
+ * @line_number: The current line number in the file
+ * Return: The pointer to the stack
+ */
+
+stack_t *rotl(stack_t **stack,
+	      unsigned int line_number __attribute__ ((unused)))
+{
+	stack_t *head = *stack;
+
+	while (!head)
+		head = head->next;
+
+	/* stack now points to the second node */
+	*stack = (*stack)->next;
+	head->next = (*stack)->prev;
+	(*stack)->prev = NULL;
+	head->next->prev = head;
+	head = head->next;
+	head->next = NULL;
+
+	return (*stack);
+}
